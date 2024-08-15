39042a521fa74bf12eea53e33911f95fbe60c642
BillStephens2022 <two4onebill@yahoo.com>
2024-08-15 09:09:32 -0400

    added Logo component and added to app layout header

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Logo from "./components/Logo";
 import Navigation from "./components/Navigation";
 
 export const metadata = {
@@ -9,6 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <header>
+          <Logo />
+        </header>
         <Navigation />
         <main>{children}</main>
       </body>
