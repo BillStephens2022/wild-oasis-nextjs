4d3b8928a880e64a2ec9afb5612da97360bcda08
BillStephens2022 <two4onebill@yahoo.com>
2024-08-16 08:00:16 -0400

    updated navbar and layout, updated styles

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -24,12 +25,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
         <footer>Copyright &copy;2024 The Wild Oasis LLC</footer>
       </body>
     </html>
