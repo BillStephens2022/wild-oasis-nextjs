e3fa229bdff2734f59ebb3920a6d83d8d339578c
BillStephens2022 <two4onebill@yahoo.com>
2024-08-19 09:51:18 -0400

    added filtering to cabins page to allow for filtering cabins by max capacity

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import CabinsList from "@/app/_components/CabinsList";
 import { Suspense } from "react";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "../_components/Filter";
 
 // revalidate every hour
 export const revalidate = 3600;
@@ -9,7 +10,9 @@ export const metadata = {
   title: "Cabins",
 };
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +26,15 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinsList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      {/* the key prop in the Suspense component below is important to force re-render if the 
+      filter changes. Without the key prop, users will not see the loading spinner if they 
+      change the filter and it takes a few seconds to update - it would appear as if the filter 
+      isn't working, when in fact it is but it may take a few seconds to refresh the data */}
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinsList filter={filter} />
       </Suspense>
     </div>
   );
