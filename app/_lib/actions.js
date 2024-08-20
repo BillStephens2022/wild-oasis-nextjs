c92a1bd47c9551a25882a184c0a5a6098a94f90b
BillStephens2022 <two4onebill@yahoo.com>
2024-08-20 17:20:18 -0400

    added signout action and wired up signout button

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -5,3 +5,7 @@ import { signIn } from "@/app/_lib/auth";
 export async function signInAction() {
     await signIn("google", { redirectTo: "/account" });
 }
+
+export async function signoutAction() {
+    await signOut({ redirectTo: "/" });
+}
