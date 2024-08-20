a8650e7a661733c62abe4cd7151c8918b5cd8b2d
BillStephens2022 <two4onebill@yahoo.com>
2024-08-20 09:15:44 -0400

    added getting user session, getting google avatar for logged in user, added conditional rendering of reservation for based on session

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
+// Dynamically determine the port and set the NEXTAUTH_URL accordingly
+const getNextAuthUrl = () => {
+  const defaultPort = 3000;
+  const port = process.env.PORT || defaultPort;
+  return `http://localhost:${port}`;
+};
+
 const authConfig = {
   providers: [
     Google({
@@ -8,6 +15,15 @@ const authConfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl.startsWith(getNextAuthUrl()) ? baseUrl : getNextAuthUrl();
+    },
+  },
 };
 
-export const { auth, handlers: {  GET, POST } } = NextAuth(authConfig);
+export const {
+  auth,
+  handlers: { GET, POST },
+} = NextAuth(authConfig);
