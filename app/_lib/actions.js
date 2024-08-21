287628cf456dcce5325c8c52f3a5d2d4f7dc9fc3
BillStephens2022 <two4onebill@yahoo.com>
2024-08-21 07:55:40 -0400

    added functionality to update user profile data

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,31 @@
 "use server";
 
-import { signIn, signOut } from "@/app/_lib/auth";
+import { auth, signIn, signOut } from "@/app/_lib/auth";
+import { supabase } from "@/app/_lib/supabase";
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  console.log('Session from updateGuest action:', session);
+  if (!session)
+    throw new Error("You need to be signed in to update your profile");
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid national ID");
+  const updateData = { nationality, countryFlag, nationalID };
+
+  const { data, error } = await supabase
+    .from('guests')
+    .update(updateData)
+    .eq('id', session.user.guestId)
+
+  if (error) {
+    console.error('Supabase error:', error);
+    throw new Error('Guest could not be updated');
+  } 
+  
+  return data;
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -9,3 +34,5 @@ export async function signInAction() {
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
 }
+
+
